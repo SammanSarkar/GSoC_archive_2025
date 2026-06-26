d289dff27e79f1932698bdb34e5d9ec7cab015ff
Karina Bataeva <karinabataeva@Karinas-MacBook-Air.local>
2026-06-26 19:51:56 +0530

    Improve GSoC resources with real curated links

---
+++ data/resources.ts
@@ -0,0 +1,35 @@
+export const resources = {
+  blogs: [
+    {
+      title: "Google Summer of Code Official Guide",
+      link: "https://summerofcode.withgoogle.com/how-it-works/",
+    },
+    {
+      title: "GSoC Student Guide (Google Developers)",
+      link: "https://developers.google.com/open-source/gsoc",
+    },
+    {
+      title: "GSoC Preparation Tips (Medium)",
+      link: "https://medium.com/tag/google-summer-of-code",
+    },
+  ],
+
+  organizations: [
+    "LLVM",
+    "Mozilla",
+    "Apache Foundation",
+    "CNCF",
+    "SymPy",
+  ],
+
+  videos: [
+    {
+      title: "GSoC Guide (YouTube Search)",
+      link: "https://www.youtube.com/results?search_query=gsoc+guide",
+    },
+  ],
+
+  proposals: [
+    "Accepted GSoC Proposal Archives",
+  ],
+};
